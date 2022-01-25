5aa88a0f189c88737986e4f452b18654bca619ac
SushmaKori <93771890+SushmaKori@users.noreply.github.com>
2022-01-25 13:21:40 +0530

    Add files via upload

---
+++ positive_list.py
@@ -0,0 +1,12 @@
+list1 = [12,-7,5,64,-14]
+list2 = [12,14,-95,3]
+output = []
+for i in list1:
+    if i>=0:
+        print(i,end = " ")
+for j in list2:
+    if j>=0:
+     output.append(j)
+print(output,end = " " )    
+        
+        
